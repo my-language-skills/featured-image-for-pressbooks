20d1cfb2f4702d4aedb9bb857d2005fd32a386f3
danzhik <35699225+danzhik@users.noreply.github.com>
2018-05-16 15:29:14 +0200

    Update pressbooks-featured-image.php

--- pressbooks-featured-image.php
+++ pressbooks-featured-image.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name: Featured Image for PressBooks CPTs
  * Description: Use an external image as Featured Image of your post/page, add support of thumbnails in PressBooks CPTs and add administration columns to check featured image status.
- * Version: 0.1
+ * Version: 0.1.1
  * Author: Daniil Zhitnitskii
  * Author URI: https://www.linkedin.com/in/daniil-zhitnitskii/
  */
